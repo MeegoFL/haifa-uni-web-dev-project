13bfe68e43e85ff98f9be28e9264036b17608db8
shiranbm@gmail.com <shiranbm@gmail.com@e6ee5753-437a-d7c2-4847-6734f3b5f15f>
2013-07-20 11:41:39 +0000

    Added missing files

---
+++ trunk/CreateAccount.php
@@ -0,0 +1,35 @@
+<?php
+ // Get the values and check for XSS or SQL injection
+ preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["username"]) ? $username = $_REQUEST["username"] : exit('XSS is detected!');
+ preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["password"]) ? $password = md5($_REQUEST["password"]) : exit('XSS is detected!');
+ preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["nickname"]) ? $nickname = $_REQUEST["nickname"] : exit('XSS is detected!');
+
+ // Connect to Database
+ $con = mysqli_connect('localhost','root','12345','test');
+ if (!$con)
+ {
+     die('Could not connect: ' . mysqli_error($con));
+ }
+
+ // Check if nickname already exist
+ $sql = "SELECT * FROM users WHERE nickname = '".$nickname."'";
+ $result = mysqli_query($con,$sql);
+ 
+ // If nickname exist return error
+ if(mysqli_num_rows($result) != 0) 
+ {
+     echo "<p style=\"color:red\">Nickname already in use, please choose another</p>";
+ } 
+ // If we got here insert the user details to Database
+ else 
+ {
+     $sql = "INSERT INTO users (username, password, nickname) VALUES ('".$username."','".$password."','".$nickname."')";
+     $result = mysqli_query($con,$sql);
+
+     echo "<p style=\"color:green\">User added successfully</p>";
+ }
+
+ // Close the connection
+ mysqli_close($con);
+ ?>
+
