a23151394c588cba9751a9327dbcd62794cc8f0d
shiranbm@gmail.com <shiranbm@gmail.com@e6ee5753-437a-d7c2-4847-6734f3b5f15f>
2013-07-25 22:04:59 +0000

    1. Added last active value to database - updated on each call to verifyCookie.php 2. Added option to see active users in past hour 3. Added local Chat with nickname print from cookie

---
+++ trunk/GetActiveUsers.php
@@ -0,0 +1,26 @@
+<?php
+
+ // Connect to Database
+ $mysqli = new mysqli("localhost", "root", "12345", "test");
+ if ($mysqli->connect_errno) 
+ {
+     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+ }
+
+ // Get list of active users (in last one hour)
+ $time = time()-3600;
+ $result = $mysqli->query("SELECT * FROM users WHERE lastactive > '".$time."'");
+
+ // If username doesn't exist return error
+ if($result->num_rows == 0) 
+ {
+     echo "No Online Users";
+ } 
+
+ while ($row = $result->fetch_array())
+ {
+     $nickname = $row['nickname'];
+     echo "<b>$nickname</b><br>";
+ }
+
+?>
