a5804840cff8559440677c3227b9c07fc2d45d21
shiranbm <shiranbm@gmail.com>
2013-07-25 19:46:56 +0000

    1. Login form now on main page 2. Password strength in now checked at register

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,9 +1,19 @@
 <?php
  // Get the values and check for XSS or SQL injection
  preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["username"]) ? $username = $_REQUEST["username"] : exit('XSS is detected!'); //Check why $_POST didn't work
- preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["password"]) ? $password = md5($_REQUEST["password"]) : exit('XSS is detected!');
+ preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["password"]) ? $password = ($_REQUEST["password"]) : exit('XSS is detected!');
  preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["nickname"]) ? $nickname = $_REQUEST["nickname"] : exit('XSS is detected!');
 
+ // Check password strength
+ if( strlen($password) < 6 ) { $error .= "Password too short!<br>";}
+ if( !preg_match("#[0-9]+#", $password) ) {$error .= "Password must include at least one number!<br>";}
+ if( !preg_match("#[a-z]+#", $password) ) {$error .= "Password must include at least one letter!<br>";}
+ if( !preg_match("#[A-Z]+#", $password) ) {$error .= "Password must include at least one CAPS!<br>";}
+
+ // Exit on weak password or update password to md5 if strong enough
+ if($error) exit("<br><b style=\"color:red\">Weak Password:<br>$error</b>");
+ else $password = md5($password);
+ 
  // Connect to Database
  $con = mysqli_connect('localhost','root','12345','test');
  if (!$con)
