594beff7bb13150da6ada948ccdf7489e6dab3b5
talyapar@gmail.com <talyapar@gmail.com@e6ee5753-437a-d7c2-4847-6734f3b5f15f>
2013-07-26 17:04:03 +0000

    InitGame.php: checks that users are valid to play

--- trunk/InitGame.php
+++ trunk/InitGame.php
@@ -1,15 +1,16 @@
 <?php
-
+    include 'verifyCookie.php';
+    verifyCookie()
      // Get the values and check for XSS or SQL injection
     preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["username"]) ? $nickname1 = $_REQUEST["nickname1"] : exit('XSS is detected!');
-    preg_match('/^[a-zA-Z0-9]+$/',$_REQUEST["password"]) ? $nickname2 = md5($_REQUEST["nickname2"]) : exit('XSS is detected!');
+    $nickname2 = $_COOKIE["nickname"]
 
+    $time = time() - 60;
      // Check if username exists
-    $result1 = $mysqli->query("SELECT * FROM users WHERE nickname = '$nickname1'");
-    $result2 = $mysqli->query("SELECT * FROM users WHERE nickname = '$nickname2'");
- 
+    $result = $mysqli->query("SELECT * FROM users WHERE nickname = '$nickname1' AND active_last > $time AND free_to_play = 1");
+
      // If username doesn't exist return error
-    if($result1->num_rows == 0 || $result2->num_rows == 0) 
+    if($result->num_rows == 0 ||) 
     {
         echo "<script type='text/javascript'>
                 window.alert('Suspicious Activity, access denied');
