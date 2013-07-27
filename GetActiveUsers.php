77d499c335d7fa0f8e3f4e09dd91392270275741
shiranbm <shiranbm@gmail.com>
2013-07-27 13:07:02 +0000

    1. user invited will now be moved to game page 2. at each InitGame all games sessions older then 24hr will be deleted

--- GetActiveUsers.php
+++ GetActiveUsers.php
@@ -2,6 +2,11 @@
 
 include 'verifyCookie.php';
 if( !verifyCookie() ) exit("Error: Not Logged In!");
+
+// Get the current user nickname from cookie
+$pieces    = explode('|', $_COOKIE['ArcomageCookie']);
+$mynickname = $pieces[0];
+
 // Connect to Database
 $mysqli = new mysqli("localhost", "root", "12345", "test");
 if ($mysqli->connect_errno)
@@ -13,10 +18,22 @@ if ($mysqli->connect_errno)
 $time = time()-60;
 $result = $mysqli->query("SELECT * FROM users WHERE last_active > '".$time."'");
 
-// If username doesn't exist return error
+// Check if nickname is in games list meaning he was invited to a game
+$invites = $mysqli->query("SELECT * FROM games WHERE nickname = '".$mynickname."'");
+
+// If no users are active return message
 if($result->num_rows == 0)
 {
     echo "No Online Users";
+}
+
+$game_session = $invites->fetch_array();
+$last_active = $game_session['last_active'];
+
+// If no user is in game list -> move him to game.php
+if($invites->num_rows > 0 && ($last_active > time()-60))
+{
+    echo "window.location.href='Game.php';";
 }
 
 $userlist = array();
--- InitGame.php
+++ InitGame.php
@@ -1,6 +1,7 @@
 <?php
 include 'verifyCookie.php';
 verifyCookie();
+
 // Get the values and check for XSS or SQL injection
 preg_match('/^[a-zA-Z0-9]+$/', $_REQUEST["nickname1"]) ? $nickname1 = $_REQUEST["nickname1"] : exit('XSS is detected!');
 $pieces    = explode('|', $_COOKIE['ArcomageCookie']);
@@ -12,6 +13,9 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+// Clear Game sessions not active for last 24hr
+$deleteLastActive = time() - 86400;
+$mysqli->query("DELETE FROM games WHERE last_active < '$deleteLastActive'");
 
 $time   = time() - 60;
 // Check if username exists
@@ -22,7 +26,7 @@ if ($result->num_rows == 0) {
     exit("window.alert('Suspicious Activity, access denied');\nwindow.location.href='index.html';");
 }
 
-
+// Set users as busy since they going into a game
 $mysqli->query("UPDATE users SET free_to_play = 0 WHERE nickname = '$nickname1' OR nickname = '$nickname2'");
 
 for ($i = 1, $game_id = 0; $game_id == 0; $i++) {
@@ -32,13 +36,16 @@ for ($i = 1, $game_id = 0; $game_id == 0; $i++) {
     }
 }
 
+$time = time();
 $first_turn = rand(0, 1);
-$mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id)
-        VALUES ('$game_id', '$nickname1', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ")");
+$success = $mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id, last_active)
+        VALUES ('$game_id', '$nickname1', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " .$time. ")");
+
+while (!$success);
 
 $first_turn = !$first_turn;
-$mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id)
-        VALUES ('$game_id', '$nickname2', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ")");
+$mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id, last_active)
+        VALUES ('$game_id', '$nickname2', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " .$time. ")");
 
 echo "window.location.href='Game.php';";
 ?>
