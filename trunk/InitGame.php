a8cb43dc33aeb8541e3e3e8170581b29f20758ab
shiranbm@gmail.com <shiranbm@gmail.com@e6ee5753-437a-d7c2-4847-6734f3b5f15f>
2013-07-27 15:20:05 +0000

    1. Added use of Sessions 2. Get users game stat

--- trunk/InitGame.php
+++ trunk/InitGame.php
@@ -1,11 +1,11 @@
 <?php
 include 'verifyCookie.php';
 verifyCookie();
+session_start();
 
 // Get the values and check for XSS or SQL injection
 preg_match('/^[a-zA-Z0-9]+$/', $_REQUEST["nickname1"]) ? $nickname1 = $_REQUEST["nickname1"] : exit('XSS is detected!');
-$pieces    = explode('|', $_COOKIE['ArcomageCookie']);
-$nickname2 = $pieces[0];
+$nickname2 = $_SESSION['nickname'];
 
 $mysqli = new mysqli("localhost", "root", "12345", "test");
 // Check connection
@@ -36,16 +36,24 @@ for ($i = 1, $game_id = 0; $game_id == 0; $i++) {
     }
 }
 
+$_SESSION['game_id'] = $game_id;
+
 $time = time();
 $first_turn = rand(0, 1);
+$success = 0;
+while (!$success)
+{
 $success = $mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id, last_active)
         VALUES ('$game_id', '$nickname1', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " .$time. ")");
+}
 
-while (!$success);
-
+$success = 0;
+while (!$success)
+{
 $first_turn = !$first_turn;
-$mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id, last_active)
+$success = $mysqli->query("INSERT INTO games (game_id, nickname, current_flag, card1_id, card2_id, card3_id, card4_id, card5_id, card6_id, last_active)
         VALUES ('$game_id', '$nickname2', '$first_turn', " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " . rand(1, 102) . ", " .$time. ")");
+}
 
 echo "window.location.href='Game.php';";
 ?>
