ed8bae4d84e6b37602ab0777fd0659eefe471d42
shiranbm <shiranbm@gmail.com>
2013-08-02 16:22:22 +0000

    1. reformat code 2. WIP on on game end

--- ResetDB.php
+++ ResetDB.php
@@ -61,7 +61,7 @@ $query = "CREATE TABLE games(
     last_played_card INT(10) DEFAULT 0,
     current_flag TINYINT(1) DEFAULT 0,
     cards_played BIGINT(20) DEFAULT 0,
-    discard_turn TINYINT(1) DEFAULT 0,
+    game_end_status INT(10) DEFAULT 0,
     last_active BIGINT(20) DEFAULT 0,
     PRIMARY KEY (`nickname`)
 );";
