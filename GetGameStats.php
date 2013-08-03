bb060a7a67c146947d9a195e8b8e12567da11a23
shiranbm <shiranbm@gmail.com>
2013-08-03 16:38:38 +0000

    1. Fixed delete game session at end of Game 2. surrender works 3. end_game now update players statistics (still not opponent)

--- GetGameStats.php
+++ GetGameStats.php
@@ -14,22 +14,16 @@ if ($mysqli->connect_errno)
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
+// If we got here it means the game has ended successfully - remove it from table
+$result = $mysqli->query("DELETE FROM games WHERE nickname = '$mynickname' AND game_end_status != '0';");
+if (!$result) exit("Remove game seesion SQL failed: (" . $mysqli->errno . ") " . $mysqli->error);
+
 // Get User information from db
 $result = $mysqli->query("SELECT * FROM users WHERE nickname = '$mynickname'");
-if (!$result) {
-    echo "Get user information SQL failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
+if (!$result) echo "Get user information SQL failed: (" . $mysqli->errno . ") " . $mysqli->error;
 
-//$_SESSION['game_id'] = $game_session['game_id'];
-$my_game_stat = $result->fetch_array();
-
-// If we got here it means the game has ended successfully - remove it from table
-$game_id = $my_game_stat['game_id'];
-$my_result = $mysqli->query("DELETE FROM games WHERE game_id = '$game_id' AND game_end_status != '0';");
-if (!$my_result) {
-    exit("Remove game seesion SQL failed: (" . $mysqli->errno . ") " . $mysqli->error);
-}
+$my_user_stat = $result->fetch_array();
 
 // Return variable with list of user's values
-echo "var userGameStat = " .json_encode($my_game_stat). ";";
+echo "var userGameStat = " .json_encode($my_user_stat). ";";
 ?>
