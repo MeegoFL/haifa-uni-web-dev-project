d000910b4cc1485527afcf39ff0daec5aec6f07d
shiranbm <shiranbm@gmail.com>
2013-08-03 19:32:38 +0000

    1. Made simple PostGame.php (kept copy of original) 2. should think of better way to check if last game win / lose

--- GetGameStats.php
+++ GetGameStats.php
@@ -14,6 +14,11 @@ if ($mysqli->connect_errno)
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
+// Get user's game result from table
+$result = $mysqli->query("SELECT game_end_status FROM games WHERE nickname = '$mynickname';");
+if (!$result) exit("User's game result SQL failed: (" . $mysqli->errno . ") " . $mysqli->error);
+$user_result = $result->fetch_array()[0];
+
 // If we got here it means the game has ended successfully - remove it from table
 $result = $mysqli->query("DELETE FROM games WHERE nickname = '$mynickname' AND game_end_status != '0';");
 if (!$result) exit("Remove game seesion SQL failed: (" . $mysqli->errno . ") " . $mysqli->error);
@@ -25,5 +30,5 @@ if (!$result) echo "Get user information SQL failed: (" . $mysqli->errno . ") " 
 $my_user_stat = $result->fetch_array();
 
 // Return variable with list of user's values
-echo "var userGameStat = " .json_encode($my_user_stat). ";";
+echo "var userGameStat = " .json_encode($my_user_stat). ";\nvar result = $user_result;";
 ?>
