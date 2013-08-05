ac6b5dc68e7824c48c94ef11f7adf9f83dbb592d
shiranbm@gmail.com <shiranbm@gmail.com@e6ee5753-437a-d7c2-4847-6734f3b5f15f>
2013-08-05 15:40:38 +0000

    1. added missing active games

---
+++ trunk/GetActiveGames.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'verifyCookie.php';
+if( !verifyCookie() ) exit("Error: Not Logged In!");
+session_start();
+
+// Connect to Database
+$mysqli = new mysqli("localhost", "root", "12345", "test");
+if ($mysqli->connect_errno) echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+
+// Get list of active games (in last one min)
+$time = time()-60;
+$stmt = $mysqli->prepare("SELECT * FROM games WHERE last_active > ?)");
+$stmt->bind_param('s', $time);
+$stmt->execute();
+$result = $stmt->get_result();
+
+// If no games are active return message
+if($result->num_rows == 0)
+{
+    echo "EMPTY";
+    exit();
+}
+
+$gameslist = array();
+while ($row = $result->fetch_array())
+{
+    $game_id = $row['game_id'];
+    $nickname1 = $row['nickname'];
+    $nickname2 = $result->fetch_array()['nickname'];
+    $gameslist[] = array($game_id, $nickname1, $nickname2);
+}
+
+echo json_encode($gameslist);
+$mysqli->close();
+?>
